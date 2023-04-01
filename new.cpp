093934e0502447aa4dfd1132ee8111206c4a8b37
Nitish Sinha <iamnitish57@gmail.com>
2023-04-01 15:19:29 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void FizzBuzz(int s){
+    if(s%3==0){
+        cout<<"Fizz";
+    }
+    if(s%4==0){
+        cout<<"Buzz";
+        
+    }
+    if(s%3 && s%4){
+        cout<<"FizzBuzz";
+    }
+
+    return;
+
+}
+
+
+int main(){
+    int n, sum=0;
+    cin>>n;
+    int arr[n];
+    for(int i =0; i<n;i++){
+        cin>>arr[i];
+        sum = sum+arr[i];
+    }
+
+    FizzBuzz(sum);
+}
